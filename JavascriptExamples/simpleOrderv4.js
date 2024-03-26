b7020ebda0047c80d43357799e32826c09813712
belactriple9 <51460755+belactriple9@users.noreply.github.com>
2024-03-26 16:16:43 -0400

    update to fix imports + note on chainId in the domain.

--- JavascriptExamples/simpleOrderv4.js
+++ JavascriptExamples/simpleOrderv4.js
@@ -1,7 +1,7 @@
 // add "type": "module" to your package.json to run this with node or name the file with extension .mjs to prevent writing existing .js files
 
 import { LimitOrder, MakerTraits, Address } from "@1inch/limit-order-sdk"
-import { Wallet } from 'ethers'
+import { Wallet, JsonRpcProvider, Contract } from 'ethers'
 import { Api, getLimitOrderV4Domain } from "@1inch/limit-order-sdk"
 import { AxiosProviderConnector } from '@1inch/limit-order-sdk/axios';
 import 'dotenv/config';
@@ -21,24 +21,27 @@ const erc20AbiFragment = [
     const expiresIn = 120n // 2m
     const expiration = BigInt(Math.floor(Date.now() / 1000)) + expiresIn
 
+    const makerAsset = "0x55d398326f99059fF775485246999027B3197955"; // BUSD
+    const makingAmount = 1_000000n; // 1 BUSD
+
     //Orders must call the approve function prior to being submitted
     // Initialize ethers provider
-    const provider = new JsonRpcProvider("http://ethereum-rpc.publicnode.com");
+    const provider = new JsonRpcProvider("http://cloudflare-eth.com/");
     const makerWallet = maker.connect(provider);
 
     // Approve the makerAsset contract to spend on behalf of the maker
     const makerAssetContract = new Contract(makerAsset, erc20AbiFragment, makerWallet);
     const domain = getLimitOrderV4Domain(chainId);
 
-    console.log('Approving makerAsset spend...', domain.verifyingContract, makerAsset);
-    try {
-        const approveTx = await makerAssetContract.approve(domain.verifyingContract, makingAmount);
-        await approveTx.wait(); // Wait for the transaction to be mined
-        console.log('Approval successful');
-    } catch (error) {
-        console.error('Error in approving makerAsset spend:', error);
-        return { success: false, reason: "Failed to approve makerAsset spend." };
-    }
+    // console.log('Approving makerAsset spend...', domain.verifyingContract, makerAsset);
+    // try {
+    //     const approveTx = await makerAssetContract.approve(domain.verifyingContract, makingAmount);
+    //     await approveTx.wait(); // Wait for the transaction to be mined
+    //     console.log('Approval successful');
+    // } catch (error) {
+    //     console.error('Error in approving makerAsset spend:', error);
+    //     return { success: false, reason: "Failed to approve makerAsset spend." };
+    // }
 
     // see MakerTraits.ts
     const makerTraits = MakerTraits.default()
@@ -47,7 +50,7 @@ const erc20AbiFragment = [
         .allowMultipleFills(); // And assuming multiple fills are also okay
 
     const order = new LimitOrder({
-        makerAsset: new Address('0x55d398326f99059fF775485246999027B3197955'), //BUSD
+        makerAsset: new Address('0x55d398326f99059fF775485246999027B3197955'), 
         takerAsset: new Address('0x111111111117dc0aa78b770fa6a738034120c302'), //1INCH
         makingAmount: 1_000000n, // 1 USDT
         takingAmount: 1_00000000000000000n, // 10 1INCH
@@ -57,15 +60,16 @@ const erc20AbiFragment = [
     }, makerTraits)
 
     const typedData = order.getTypedData(domain)
+    const converted = {...typedData.domain, chainId: '1'} // convert chainId to string, because ethers wants a bignumberish value
     const signature = await maker.signTypedData(
-        typedData.domain,
+        converted,
         { Order: typedData.types.Order },
         typedData.message
     )
 
     const api = new Api({
         networkId: chainId, // ethereum
-        authKey: String(process.env.API_KEY), // get it at https://portal.1inch.dev/
+        authKey: String(process.env.INCH_API_KEY), // get it at https://portal.1inch.dev/
         httpConnector: new AxiosProviderConnector()
     });
 
